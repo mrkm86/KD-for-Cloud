ca674f84da33342c81d4d7848b9fc60f75a5f399
keisuke <murakami@heartis-sc.co.jp>
2018-09-05 09:19:14 +0900

    JOB修正②

--- DDL/CREATE_OBJECT_KD.sql
+++ DDL/CREATE_OBJECT_KD.sql
@@ -494,7 +494,7 @@ create or replace PACKAGE BODY  "PKG_KD_DELETE_OLD_DATA" IS
         DBMS_SCHEDULER.CREATE_JOB( 
             job_name => 'JOB_KD_DELETE_OLD_DATA',  
             job_type => 'STORED_PROCEDURE',  
-            job_action => 'PKG_DELETE_OLD_DATA.fnc_DeleteOldData',  
+            job_action => 'PKG_KD_DELETE_OLD_DATA.fnc_DeleteOldData',  
             start_date => TO_TIMESTAMP_TZ(CONCAT(TO_CHAR(CURRENT_DATE, 'yyyy/MM/dd'), ' 00:00:00 +09:00'),'yyyy/mm/dd hh24:mi:ss TZH:TZM'),
             repeat_interval => 'FREQ=DAILY;',  
             auto_drop => FALSE, enabled => TRUE); 
